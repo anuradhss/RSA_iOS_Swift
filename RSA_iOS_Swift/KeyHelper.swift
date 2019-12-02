194eb09ea293562ed6539adc4d6bc0641a8a2269
Chanaka Caldera <anuradhtuck@gmail.com>
2019-12-02 16:47:22 +0530

    Adding Sign With Private Key function and demo

--- RSA_iOS_Swift/KeyHelper.swift
+++ RSA_iOS_Swift/KeyHelper.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CommonCrypto
 
 struct KeyHelper {
     private var osstatus: OSStatus?
@@ -14,9 +15,10 @@ struct KeyHelper {
     private var privatekey: SecKey?
 }
 
+//MARK: - Generating RSA Key Pair
 extension KeyHelper {
     
-    mutating func generateRSAKeyPair(length: Int) {
+    mutating func generateRSAKeyPair(length: Int, completion: @escaping(String?, String?) -> ()) {
         
         let publicKeyAttributes: [NSObject: NSObject] = [
             kSecAttrIsPermanent: true as NSObject,
@@ -45,8 +47,92 @@ extension KeyHelper {
             
             var _publickey: AnyObject?
             var _privatekey: AnyObject?
+            
+            let statusPublic: OSStatus = SecItemCopyMatching(publicKeyAttributes as CFDictionary, &_publickey)
+            let statusPrivate: OSStatus = SecItemCopyMatching(privateKeyAttributes as CFDictionary, &_privatekey)
+            
+            if statusPublic == noErr && statusPrivate == noErr {
+                if let _publickeydata = _publickey as? Data, let _privatekeydata = _privatekey as? Data {
+                    
+                    completion(_publickeydata.base64EncodedString(), _privatekeydata.base64EncodedString())
+                    return
+                }
+            }
+            completion(nil, nil)
+            return
         }
-        
-        
+        completion(nil, nil)
+        return
     }
 }
+
+//MARK: - Sign With Private Key
+extension KeyHelper {
+    func signTextWithPrivateKey(key privatekeystring: String,withtext text: String, completion: @escaping(String?) -> ()) {
+        
+        if let _privatekeyData: Data = Data(base64Encoded: privatekeystring) {
+            
+            let keyDictionary: [NSObject: NSObject] = [
+                kSecAttrKeyType: kSecAttrKeyTypeRSA,
+                kSecAttrKeyClass: kSecAttrKeyClassPrivate,
+                kSecAttrKeySizeInBits: 2048 as NSObject,
+                kSecReturnPersistentRef: true as NSObject
+            ]
+            
+            var error: Unmanaged<CFError>?
+            
+            let _privateSecKey: SecKey = SecKeyCreateWithData(_privatekeyData as CFData, keyDictionary as CFDictionary, nil)!
+            
+            if error != nil {
+                print(error ?? "Error in Signed Process")
+                completion(nil)
+                return
+            }
+
+            guard let signedText = self.signUsingPrivate(text, _privateSecKey) else {
+                completion(nil)
+                return
+            }
+            completion(signedText)
+            return
+        }
+    }
+}
+
+//MARK: - Sign the given text
+extension KeyHelper {
+    private func signUsingPrivate(_ text: String, _ key: SecKey) -> String? {
+        var digest = Data(count: Int(CC_SHA256_DIGEST_LENGTH))
+        let data = text.data(using: .utf8)!
+
+        let _ = digest.withUnsafeMutableBytes { digestBytes in
+            data.withUnsafeBytes { dataBytes in
+                CC_SHA256(dataBytes, CC_LONG(data.count), digestBytes)
+            }
+        }
+
+        var signature = Data(count: SecKeyGetBlockSize(key) * 4)
+        var signatureLength = signature.count
+
+        let result = signature.withUnsafeMutableBytes { signatureBytes in
+            digest.withUnsafeBytes { digestBytes in
+                SecKeyRawSign(key,
+                              SecPadding.PKCS1SHA256,
+                              digestBytes,
+                              digest.count,
+                              signatureBytes,
+                              &signatureLength)
+            }
+        }
+
+        let count = signature.count - signatureLength
+        signature.removeLast(count)
+
+        guard result == noErr else {
+            print("Error Occured")
+            return nil
+        }
+        return signature.base64EncodedString()
+    }
+}
+
--- RSA_iOS_Swift/ViewController.swift
+++ RSA_iOS_Swift/ViewController.swift
@@ -9,12 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var signedPayload: String?
+    private var signedStatus: Bool?
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        generateKeyPairAndSignWithPrivateKey { [weak self] status in
+            self?.signedStatus = status
+            print("Signing Process : ", self?.signedStatus ?? false)
+            return
+        }
     }
-
-
 }
 
+//MARK: - Generate Key Pair and Sign With Private Key
+extension ViewController {
+    func generateKeyPairAndSignWithPrivateKey(completion: @escaping(Bool) -> ()) {
+        
+        generateKeyPair { [weak self] (_pubKey, _priKey) in
+            
+            if _pubKey != nil && _priKey != nil {
+                self?.signWithPrivateKey(privatekeyString: _priKey!, completion: { [weak self] _signed in
+                    
+                    if _signed != nil {
+                        self?.signedPayload = _signed
+                        print("Signed : ", self?.signedPayload ?? " ")
+                        completion(true)
+                        return
+                    }
+                    print("Error Occured While Signing With Private Key")
+                    completion(false)
+                    return
+                })
+                return
+            }
+            print("Error Occured While Generating Key Pair")
+            completion(false)
+            return
+        }
+        return
+    }
+}
+
+// MARK: - Use of generating key pair by invokine generate key pair method in KyeHelper
+extension ViewController {
+    
+    private func generateKeyPair(completion: @escaping(String?, String?) ->()) {
+        
+        var _keyhelper = KeyHelper()
+        
+        _keyhelper.generateRSAKeyPair(length: 2048, completion: { (_publickKey, _privateKey) in
+            
+            if _publickKey != nil && _privateKey != nil {
+                completion(_publickKey, _privateKey)
+                return
+            }
+            completion(nil, nil)
+            return
+        })
+        return
+    }
+}
+
+//MARK: - Sign With Private Key
+extension ViewController {
+    private func signWithPrivateKey(privatekeyString: String, completion: @escaping(String?) ->()) {
+        
+        let _keyhelper = KeyHelper()
+        
+        _keyhelper.signTextWithPrivateKey(key: privatekeyString, withtext: "Hello Anuradh", completion: { (_signed) in
+            
+            if _signed != nil {
+                completion(_signed)
+                return
+            }
+            completion(nil)
+            return
+        })
+        return
+    }
+}
